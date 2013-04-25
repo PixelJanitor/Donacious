2f5d8481847ee38518f07e01b951458cdb38eaf5
Alex Burkhart <saterus@gmail.com>
2013-04-25 17:35:36 -0400

    Generated rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Donacious::Application.config.secret_key_base = 'ecfc50502f2c5ae8bb3baea2b809ba724f48f4c16af595bae4634bac1ed43ebbaf06fcc60c9dc25e65d3c80e225ea81e5ff2db411952ba154431ceed5d60a3da'
